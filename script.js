d2f593de5795a06f90fa801bac995d4e945c3ebc
jcap-debug <jose.asto.p@tecsup.edu.pe>
2026-09-20 20:50:01 -0500

    Add iniciar function to display message and scale element

---
+++ script.js
@@ -0,0 +1,7 @@
+function iniciar() {
+    document.getElementById("mensaje").innerHTML =
+        "Tengo algo especial para ti 💛";
+
+    document.querySelector(".girasol").style.transform =
+        "scale(1.3)";
+}
